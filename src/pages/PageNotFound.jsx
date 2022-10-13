56db6f0c3f8f78899fe8a4b4276ea958c5b1da50
kriptonian1 <shymal.sawan@gmail.com>
2022-10-13 15:41:06 +0530

    feat: add 404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -5,21 +5,18 @@ import { Navbar } from '../components'
 const PageNotFound = () => {
 
     const navigate = useNavigate()
- 
+
     return (
         <div>
             <Navbar />
-            <div className='font-clashDisplay text-6xl text-left flex flex-col justify-center items-center h-[80vh]'>
-                <div className='m-10'>
-                    <div>Oops!!!</div>
-                    <div>You are not supposed to found</div>
-                    <div>this page</div>
-                    <div>404</div>
-                </div>
+            <div className='font-clashDisplay text-left flex flex-col justify-center items-center '>
 
-                <button onClick={() => navigate("/")} className='bg-[#2F80ED] text-base w-[10rem] hover:bg-[#2E8EEE] py-3 px-5 rounded-full font-semibold'>
-                    Go back Home
-                </button>
+                <div className='text-[15rem] font-clashDisplay font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#3F51B1] via-[#A86AA4]  to-[#FFCB70]'>
+                    404
+                </div>
+                <div className='text-[24rem] text-transparent shadow shadow-white'>
+                    Oops
+                </div>
 
             </div>
         </div>
